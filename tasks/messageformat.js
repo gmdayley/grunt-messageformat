e18d6e6a8d3798fd8417158628cb3a07e4d80953
Chad Maughan <chadmaughan@gmail.com>
2013-05-10 09:36:34 -0600

    task testing

--- tasks/messageformat.js
+++ tasks/messageformat.js
@@ -8,45 +8,57 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+var messageformat = require('messageformat');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+module.exports = function (grunt) {
 
-  grunt.registerMultiTask('messageformat', 'Compile MessageFormat.js files.', function() {
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    grunt.registerMultiTask('messageformat', 'Compile MessageFormat.js files.', function () {
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (file) {
+
+            // Concat specified files.
+            var src = file.src.filter(function (filepath) {
+
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                }
+                else {
+                    return true;
+                }
+
+            }).map(function (filepath) {
+
+                    // Read file source.
+                    return grunt.file.read(filepath);
+
+                }).map(function(filepath) {
+
+                    // get the locale from the file name (assuming format example of en.json)
+                    grunt.log.writeln(filepath);
+
+                })
+                .join(grunt.util.normalizelf(options.separator));
+
+            // Handle options.
+            src += options.punctuation;
+
+            // Write the destination file.
+            grunt.file.write(file.dest, src);
+
+            // Print a success message.
+            grunt.log.writeln('File "' + file.dest + '" created.');
+        });
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
 };
